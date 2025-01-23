c65c47428e9e9419259bfafb102aa4b80c7ab371
maximedervaux <maximedervaux51@gmail.com>
2025-01-23 11:48:49 +0100

    Char qui tourne sur lui meme et tire sur les cibles détéctés

--- src/main.c
+++ src/main.c
@@ -3,14 +3,13 @@
 #include "stdlib.h"
 #include "unistd.h"
 
-// Afficher les informations du joueur
 void afficher_infos_player(BC_Connection *connection) {
     BC_PlayerData monPlayer = bc_get_player_data(connection);
     printf("ID : %d\n", monPlayer.id);
     printf("Health : %d\n", monPlayer.health);
     printf("Armor : %d\n", monPlayer.armor);
     printf("Score : %d\n", monPlayer.score);
-    printf("Position x: %.2f , y: %.2f\n", monPlayer.position.x, monPlayer.position.y);
+    printf("Position x: %d , y: %d\n", monPlayer.position.x, monPlayer.position.y);
 }
 
 
@@ -23,12 +22,11 @@ void radar_ping(BC_Connection *connection) {
     BC_List *objects = bc_radar_ping(connection);
     while (objects) {
         BC_MapObject *object = (BC_MapObject *)bc_ll_value(objects);
-        printf("Objet détecté : ID=%d, Type=%d, Position=(%.2f, %.2f, %.2f)\n",
+        printf("Objet détecté : ID=%d, Type=%d, Position=(%.2f, %.2f)\n",
                object->id,
                object->type,
                object->position.x,
-               object->position.y,
-               object->position.z);
+               object->position.y);
         objects = bc_ll_next(objects);
     }
 }
@@ -42,17 +40,50 @@ int main() {
     }
     printf("Connecté au serveur avec succès !\n");
 
+    BC_WorldInfo info = bc_get_world_info(connection);
+
     afficher_infos_player(connection);
 
     printf("Déplacement du joueur...\n");
-    se_deplacer(connection, 1.0f, 0.0f, 0.0f);  // Déplacer vers la droite
+    se_deplacer(connection, 0.0, 0.0, 0.0);
 
 
-    printf("Analyse radar...\n");
-    radar_ping(connection);
+    afficher_infos_player(connection);
+
+	int angle = 0;
+	int angle_increment = 1;
+	int cible_trouvee = 0;
+
+	while (!cible_trouvee) {
+    	sleep(3);
+   		float angle_radian = angle * (3.14159 / 180);
+    	BC_ShootResult test = bc_shoot(connection, angle_radian);
+    	printf("TIR à angle %.2f radian (%d degrés) : %d\n", angle_radian, angle, test.success);
+
+    	if (test.success) {
+        	cible_trouvee = 1;
+        	printf("Cible trouvée à angle %.2f radian (%d degrés) ! Bombardement en cours...\n", angle_radian, angle);
 
 
-    sleep(2);
+       	 while (test.success) {
+            sleep(3);
+            test = bc_shoot(connection, angle_radian);
+            printf("BOMBARDEMENT à angle %.2f radian : %d\n", angle_radian, test.success);
+        }
 
+        printf("La cible détruite ou hors de portée!\n");
+    }
+
+   	 angle += angle_increment;
+   	 if (angle >= 360) {
+   	     angle -= 360;
+   	 }
+	}
+
+    //printf("Analyse radar...\n");
+    //radar_ping(connection);
+
+
+	afficher_infos_player(connection);
     return 0;
 }
